61b5c3a58c7ae044518e5eab537f14fa00b4721e
김희수 <taitty@gmail.com>
2020-05-01 00:27:20 +0900

    add use case and protocol

---
+++ googleCalendar/googleCalendar/domain/repositories/getCalendarListRepository.swift
@@ -0,0 +1,15 @@
+//
+//  getCalendarListRepository.swift
+//  googleCalendar
+//
+//  Created by 김희수 on 2020/05/01.
+//  Copyright © 2020 taitty. All rights reserved.
+//
+
+import Foundation
+
+protocol getCalendarListRepository {
+    func getCalendarlist()
+    func getUserData()
+    func setUserData()
+}
---
+++ googleCalendar/googleCalendar/domain/usecases/getCalendarList.swift
@@ -0,0 +1,13 @@
+//
+//  getCalendarList.swift
+//  googleCalendar
+//
+//  Created by 김희수 on 2020/05/01.
+//  Copyright © 2020 taitty. All rights reserved.
+//
+
+import Foundation
+
+class getCalendarList {
+    
+}
