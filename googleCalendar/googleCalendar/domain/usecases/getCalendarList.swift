b87cc0eb41979ff1c40af7a2750b5e908a7da060
김희수 <taitty@gmail.com>
2020-05-03 02:31:14 +0900

    add test code

--- googleCalendar/googleCalendar/domain/usecases/getCalendarList.swift
+++ googleCalendar/googleCalendar/domain/usecases/getCalendarList.swift
@@ -11,13 +11,28 @@ import Foundation
 class getCalendarList {
     
     func getCalendarData() -> [String: Any]? {
+        print("\(#file) \(#line) \(#function)")
         
         return nil
     }
     
-    func getCalendarList() -> [[String: Any]]? {
+    func getCalendarList(calendarData: [String: Any]) -> [[String: Any]]? {
+        print("\(#file) \(#line) \(#function)")
         
-        return nil
+        guard let items = calendarData["items"] as? [[String: Any]] else {
+            print("failed to get items")
+            return nil
+        }
+        
+        var calendarList = [[String: Any]]()
+        for item in items {
+            var calendar = Dictionary<String, Any>()
+            calendar["id"] = item["id"]
+            calendar["summary"] = item["summary"]
+            calendarList.append(calendar)
+        }
+        
+        return calendarList
     }
     
     
--- googleCalendar/googleCalendarTests/domain/usecases/mockGetCalendarList.swift
+++ googleCalendar/googleCalendarTests/domain/usecases/mockGetCalendarList.swift
@@ -148,27 +148,8 @@ class mockGetCalendarList {
     }
     
     func mockGetCalendarList() -> [[String: Any]]? {
-        print("\(#file) \(#line) \(#function)")
-        
-        guard let json = self.mockGetCalendarData() else {
-            print("failed to get calendar data")
-            return nil
-        }
-        
-        guard let items = json["items"] as? [[String: Any]] else {
-            print("failed to get items")
-            return nil
-        }
-        
-        var calendarList = [[String: Any]]()
-        for item in items {
-            var calendar = Dictionary<String, Any>()
-            calendar["id"] = item["id"]
-            calendar["summary"] = item["summary"]
-            calendarList.append(calendar)
-        }
-        
-        return calendarList
+        return nil
     }
+
     
 }
