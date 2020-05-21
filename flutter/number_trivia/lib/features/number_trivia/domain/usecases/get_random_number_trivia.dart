64b5e77d20b1b003a7639125ae239897a74dd851
taitty <taitty@gmail.com>
2020-05-22 02:22:30 +0900

    add new flutter project

---
+++ flutter/number_trivia/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,16 @@
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/usecase/usecase.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia extends UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
