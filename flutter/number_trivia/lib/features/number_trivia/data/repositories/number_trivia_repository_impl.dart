c58c5bf0c3f75b2377145a8cb492a71c0afd4103
taitty <taitty@gmail.com>
2020-05-25 02:59:37 +0900

    TDD Course [8]

--- flutter/number_trivia/lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ flutter/number_trivia/lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:number_trivia/core/error/exception.dart';
 import 'package:number_trivia/core/error/failures.dart';
-import 'package:number_trivia/core/platform/network_info.dart';
+import 'package:number_trivia/core/network/network_info.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
