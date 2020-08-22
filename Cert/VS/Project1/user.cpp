38d49446bece258f4dd35f795a8bf43db0ab6de2
taitty <taitty@gmail.com>
2020-08-22 21:35:49 +0900

    no message

--- Cert/VS/Project1/user.cpp
+++ Cert/VS/Project1/user.cpp
@@ -8,6 +8,16 @@ int size_M;
 int tMap[MAX_N][MAX_N];
 int tStars[MAX_M][MAX_M];
 
+struct posi {
+	int x;
+	int y
+};
+
+struct stars_posi {
+	posi std;
+	posi nor[MAX_M];
+};
+
 struct Result {
 	int y, x;
 };
@@ -22,13 +32,94 @@ void init(int N, int M, int Map[MAX_N][MAX_N])
 			tMap[i][j] = Map[i][j];
 }
 
+void makeTable(int deg, int stars[MAX_M][MAX_M])
+{
+	stars_posi result;
+	int numOfstar = 0;
+
+	switch (deg) {
+	case 1://90
+		for (int i = 0; i < size_M; ++i) {
+			for (int j = size_M - 1; j > 0; --j) {
+				if (stars[i][j] == 0)
+					continue;
+
+				if (stars[i][j] == 9) {
+					result.std.x = j;
+					result.std.y = i;
+				}
+				else {
+					result.nor[numOfstar].x = j;
+					result.nor[numOfstar].y = i;
+					++numOfstar;
+				}
+			}
+		}
+		break;
+	case 2://180
+		for (int i = size_M - 1; i > 0; --i) {
+			for (int j = size_M - 1; j > 0; --j) {
+				if (stars[i][j] == 0)
+					continue;
+
+				if (stars[i][j] == 9) {
+					result.std.x = j;
+					result.std.y = i;
+				}
+				else {
+					result.nor[numOfstar].x = j;
+					result.nor[numOfstar].y = i;
+					++numOfstar;
+				}
+			}
+		}
+		break;
+	case 3://270
+		for (int i = size_M - 1; i > 0; --i) {
+			for (int j = 0; j < size_M; ++j) {
+				if (stars[i][j] == 0)
+					continue;
+
+				if (stars[i][j] == 9) {
+					result.std.x = j;
+					result.std.y = i;
+				}
+				else {
+					result.nor[numOfstar].x = j;
+					result.nor[numOfstar].y = i;
+					++numOfstar;
+				}
+			}
+		}
+		break;
+	default:// 0
+		for (int i = 0; i < size_M; ++i) {
+			for (int j = 0; j < size_M; ++j) {
+				if (stars[i][j] == 0)
+					continue;
+
+				if (stars[i][j] == 9) {
+					result.std.x = j;
+					result.std.y = i;
+				}
+				else {
+					result.nor[numOfstar].x = j;
+					result.nor[numOfstar].y = i;
+					++numOfstar;
+				}
+			}
+		}
+		break;
+	}
+}
+
 Result findConstellation(int stars[MAX_M][MAX_M])
 {
 	Result res;
 
 	for (int i = 0; i < 4; ++i) {
+		makeTable(i, starts)
 		matchConstellation();
-		rotateConstellation();
 	}
 
 	res.y = res.x = 0;
