41d3079be4f634e719135ab4aa382e364d94c315
taitty <taitty@gmail.com>
2020-09-17 01:18:32 +0900

    no message

---
+++ Cert/user.cpp
@@ -0,0 +1,9 @@
+#define MAX_PICTURE_SIZE 1000
+#define MAX_PIECE_SIZE 100
+#define MAXN 1500
+
+extern bool setPicture(int id, int x, int y);
+
+void mergePictures(int N, int M, int K, char pictures[MAXN][MAX_PIECE_SIZE][MAX_PIECE_SIZE]) {
+
+}
