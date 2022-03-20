9734b5e93f496d02e4f244d1f96e280c1943f9ed
김희수 <taitty@gmail.com>
2022-03-21 04:35:57 +0900

    app store 검색 mock data 추가

---
+++ myAppStore/myAppStore/Data/DataSource/AppStoreDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  AppStoreDataSource.swift
+//  myAppStore
+//
+//  Created by 김희수 on 2022/03/21.
+//
+
+import Foundation
---
+++ myAppStore/myAppStore/Domain/Repository/MockData/MockAppStoreData.swift
@@ -0,0 +1,15 @@
+//
+//  MockAppStoreDataSource.swift
+//  myAppStore
+//
+//  Created by 김희수 on 2022/03/21.
+//
+
+import Foundation
+import ReactiveSwift
+
+class MockAppStoreData {
+
+    private let dummy_searchModel = [
+    ]
+}
