a8f7b34c5c926ef17600c0af279213eea40beaee
김희수 <taitty@gmail.com>
2022-03-30 03:30:05 +0900

    apply scrollview

---
+++ myScrollView/myScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myScrollView
+//
+//  Created by 김희수 on 2022/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
