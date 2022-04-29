8381cb8d85ff5c8546b015f85a36807d574de02b
김희수 <taitty@gmail.com>
2022-04-29 22:46:53 +0900

    start new project to improve myPhotoBrowser

---
+++ myPhotoBrowser2/myPhotoBrowser2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPhotoBrowser2
+//
+//  Created by 김희수 on 2022/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
