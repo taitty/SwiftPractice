56b0cddf619bee93998bb3dfaa8821240720bca2
김희수 <taitty@gmail.com>
2023-09-24 22:41:41 +0900

    add myTrancCoding

---
+++ myTransCoding/myTransCoding/ViewController.swift
@@ -0,0 +1,134 @@
+//
+//  ViewController.swift
+//  myTransCoding
+//
+
+import UIKit
+import AVKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        showImagePickerOptions()
+    }
+    
+    func imagePicker(sourceType: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        return imagePicker
+    }
+    
+    func showImagePickerOptions() {
+        let alertVC = UIAlertController(title: "Pick a Photo",
+                                        message: "Choose a Picture",
+                                        preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] (action) in
+            guard let self = self else { return }
+            let cameraImagePicker = self.imagePicker(sourceType: .camera)
+            cameraImagePicker.delegate = self
+            self.present(cameraImagePicker, animated: true) {
+                
+            }
+        }
+        
+        let libraryAction = UIAlertAction(title: "Library", style: .default) { [weak self] (action) in
+            guard let self = self else { return }
+            let libraryImagePicker = self.imagePicker(sourceType: .photoLibrary)
+            libraryImagePicker.delegate = self
+            libraryImagePicker.mediaTypes = ["public.image", "public.movie"]
+            self.present(libraryImagePicker, animated: true) {
+                
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertVC.addAction(cameraAction)
+        alertVC.addAction(libraryAction)
+        alertVC.addAction(cancelAction)
+        self.present(alertVC, animated: true)
+    }
+    
+    func encodeVideo(videoURL: URL){
+        let avAsset = AVURLAsset(url: videoURL)
+        let startDate = Date()
+        let exportSession = AVAssetExportSession(asset: avAsset, presetName: AVAssetExportPresetPassthrough)
+        
+        let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let myDocPath = NSURL(fileURLWithPath: docDir).appendingPathComponent("temp.mp4")?.absoluteString
+        
+        let docDir2 = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as NSURL
+        
+        let filePath = docDir2.appendingPathComponent("rendered-Video.mp4")
+        deleteFile(filePath!)
+        
+        if FileManager.default.fileExists(atPath: myDocPath!){
+            do{
+                try FileManager.default.removeItem(atPath: myDocPath!)
+            }catch let error{
+                print(error)
+            }
+        }
+        
+        exportSession?.outputURL = filePath
+        exportSession?.outputFileType = AVFileType.mp4
+        exportSession?.shouldOptimizeForNetworkUse = true
+        
+        let start = CMTimeMakeWithSeconds(0.0, preferredTimescale: 0)
+        let range = CMTimeRange(start: start, duration: avAsset.duration)
+        exportSession?.timeRange = range
+        
+        exportSession!.exportAsynchronously{() -> Void in
+            switch exportSession!.status{
+            case .failed:
+                print("\(exportSession!.error!)")
+            case .cancelled:
+                print("Export cancelled")
+            case .completed:
+                let endDate = Date()
+                let time = endDate.timeIntervalSince(startDate)
+                print(time)
+                print("Successful")
+                print(exportSession?.outputURL ?? "")
+            default:
+                break
+            }
+            
+        }
+    }
+
+    func deleteFile(_ filePath:URL) {
+        guard FileManager.default.fileExists(atPath: filePath.path) else{
+            return
+        }
+        do {
+            try FileManager.default.removeItem(atPath: filePath.path)
+        }catch{
+            fatalError("Unable to delete file: \(error) : \(#function).")
+        }
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imageType = info[.mediaType] as? String
+        if (imageType == "public.image") {
+            let image = info[.originalImage] as! UIImage
+            self.imageView.image = image
+        } else {
+            print("\(info[.mediaURL])")
+            print("\(info[.referenceURL])")
+            
+            encodeVideo(videoURL: info[.mediaURL] as! URL)
+        }
+        self.dismiss(animated: true)
+    }
+}
+
